4e88a6f0d06b691e29f95b584de05f5ceb357877
Seungmin Jeong <sontneptm2@gmail.com>
2022-05-26 13:55:26 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import styled from "styled-components";
+
+const Title = styled.h1`
+  color: red;
+`;
+
 function App() {
-  return null;
+  return (
+    <div>
+      <Title>Seungmin Jeong</Title>
+    </div>
+  );
 }
 
 export default App;
