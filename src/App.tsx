8ed034bbfc7b71ce0f509d56789470c00fba2cc4
Seungmin Jeong <sontneptm2@gmail.com>
2022-05-30 22:50:38 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,24 @@
 import styled from "styled-components";
 
-const Wrapper = styled.h1`
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
+
+const Wrapper = styled.div`
   display: flex;
   height: 100vh;
   width: 100vw;
   justify-content: center;
   align-items: center;
-  color: tomato;
-`;
-
-const Box = styled.div`
-  width: 100px;
-  height: 100px;
+  background-color: ${(props) => props.theme.backgroundColor};
 `;
 
 function App() {
   return (
     <div>
-      <Wrapper>Seungmin Jeong</Wrapper>
+      <Wrapper>
+        <Title>Seungmin Jeong</Title>
+      </Wrapper>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "styled-components";
 import App from "./App";
+
+const darkTheme = {
+  textColor: "whitesmoke",
+  backgroundColor: "#111",
+};
+
+const lightTheme = {
+  backgroundColor: "whitesmoke",
+  textColor: "#111",
+};
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
